da1b5abb7e8bffe1a50f05582288c76fe3ab4fc4
mjdude <mo@syncspot.net>
2016-05-17 14:53:21 +0100

    added default value

--- public/app.jsx
+++ public/app.jsx
@@ -14,12 +14,17 @@ var Greeter = React.createClass({
   onButtonClick: function(e){
     e.preventDefault();
 
-    var name = this.refs.name.value;
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
 
-    this.refs.name.value = '';
-    this.setState({
-      name: name,
-    });
+    if (typeof name === 'string' && name.length > 0) {
+    
+          this.setState({
+            name: name,
+          });
+    }
+
   },
   render: function () {
     var name = this.state.name;
